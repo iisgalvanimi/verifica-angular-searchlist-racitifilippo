e7dd4d4d4dd64cb0e7bab1377306cb86feeb22b5
racitifilippo <raciti.filippo@iisgalvanimi.edu.it>
2022-03-10 08:40:55 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular-Empty-Project';
+export class AppComponent implements OnInit {
+
+  frutti: string[] = ['la banana', 'la fragola', 'la mela', 'la pera', 'il cocomero']
+
+  out: string[] = this.frutti
+
+  testo: string = ""
+
+  
+
+  ngOnInit(): void {
+  }
+  
+
+  update(){
+
+    this.out = []
+
+    for (let x of this.frutti){
+      if (x.search(this.testo) != -1){
+        this.out.push(x)
+      }
+    }
+
+    
+  }
+
 }
